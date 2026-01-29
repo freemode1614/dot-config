69e0185a21a5f2ad9a1ff6cc6513f613bf0d3d55
wenpeng <wenpeng.lei@ncsi.com.cn>
2026-01-29 17:40:52 +0800

    merge: 合并代码

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,4 @@ if status is-interactive
   /Users/ray/.local/bin/mise activate fish | source
 end
 
-<<<<<<< HEAD
 /Users/wenpeng.lei/.local/bin/mise activate fish | source # added by https://mise.run/fish
-=======
->>>>>>> 385a2bf3a673aac7f9fae542cc93f12c481ab4c8
