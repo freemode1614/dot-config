5fe3173ed3f0facc11df9ce9cd91e8d34954d782
wenpeng <wenpeng.lei@ncsi.com.cn>
2026-02-23 15:17:01 +0800

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,18 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     # /Users/ray/.local/bin/mise activate fish | source
     $HOME/.local/bin/mise activate fish | source # added by https://mise.run/fish
+    set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
+    set -gx PATH /home/linuxbrew/.linuxbrew/sbin $PATH
+
 end
 
-if test -f /home/linuxbrew/.linuxbrew/etc/fish/completions/brew.fish
-    source /home/linuxbrew/.linuxbrew/etc/fish/completions/brew.fish
-    elif test -f $HOME/.linuxbrew/etc/fish/completions/brew.fish
-    source $HOME/.linuxbrew/etc/fish/completions/brew.fish
-end
-
-set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
-set -gx PATH /home/linuxbrew/.linuxbrew/sbin $PATH
-
-alias z="zellij"
+alias zj="zellij"
 alias p="pnpm"
 alias n="nvim"
 alias lg="lazygit"
